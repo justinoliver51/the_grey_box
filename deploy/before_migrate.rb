8973d89d6ee56b3b22d969e86dad689284a1d4f3
Justin Oliver <justinoliver51@yahoo.com>
2014-09-28 23:07:28 -0500

    Adding --trace to my precompile command

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -5,7 +5,7 @@ Chef::Log.info("Precompiling assets for #{rails_env}...")
   
 current_release = release_path
   
-execute "rake assets:precompile" do
+execute "rake assets:precompile --trace" do
   cwd current_release
   command "bundle exec rake assets:precompile"
   environment "RAILS_ENV" => rails_env

6ef97739126d6697fb645d162413be128fae30e7
Justin Oliver <justinoliver51@yahoo.com>
2014-09-28 22:53:21 -0500

    Trying new variable names

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -3,7 +3,7 @@ Chef::Log.info("Running deploy/before_migrate.rb...")
 rails_env = new_resource.environment["RAILS_ENV"]
 Chef::Log.info("Precompiling assets for #{rails_env}...")
   
-current_release = current_path
+current_release = release_path
   
 execute "rake assets:precompile" do
   cwd current_release
